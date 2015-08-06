27c4d7fdf730311b7fe34c2c5184dcdeba4cccdf
Sarah Tsumayoi <tsuharusarah873@gmail.com>
2015-08-06 03:20:26 -0700

    Update mobile.master.php

--- views/mobile.master.php
+++ views/mobile.master.php
@@ -1,9 +1,7 @@
 <?php
 // Copyright 2011 Toby Zerner, Simon Zerner
 // This file is part of esoTalk. Please see the included license file for usage information.
-
 if (!defined("IN_ESOTALK")) exit;
-
 /**
  * Mobile master view. Displays a simplified HTML template with a header and footer.
  *
